fd14af2919d2e72b4aeef0940bcf79824418cf6f
MIUI Russia <kirill.desirehd@gmail.com>
2012-11-25 17:00:08 +0400

    Fixed

--- set_build_prop.sh
+++ set_build_prop.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-build_prop_file=$1
+rebuild_numbers=`echo $(date +%y.%m.%d) | grep -Eo '.{7}$' | sed 's/.0/./g'`;
+build_prop_file=$1;
 # other arguments: # product=$2 # number=$3 # version=$4
 
-number=$(date +%y.%m.%d)
-rebuild_number=`echo $number | grep -Eo '.{7}$' | sed 's/.0/./g'`
 
 cat $build_prop_file | sed -e "s/ro\.build\.version\.incremental=.*/ro\.build\.version\.incremental=$rebuild_numbers/" \
                       | sed -e "s/ro\.product\.mod_device=.*//" > $build_prop_file.new
@@ -12,4 +11,3 @@ cat $build_prop_file | sed -e "s/ro\.build\.version\.incremental=.*/ro\.build\.v
 echo "ro.product.mod_device=$2" >> $build_prop_file.new
 echo "ro.skia.use_data_fonts=1" >> $build_prop_file.new
 mv $build_prop_file.new $build_prop_file
-

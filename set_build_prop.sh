e8bd50724810ec4b9e034b22f2e285c53ad0a5fa
KDGDev <kirill.desirehd@gmail.com>
2012-11-25 11:49:10 +0400

    Updated generating date

--- set_build_prop.sh
+++ set_build_prop.sh
@@ -3,7 +3,10 @@
 build_prop_file=$1
 # other arguments: # product=$2 # number=$3 # version=$4
 
-cat $build_prop_file | sed -e "s/ro\.build\.version\.incremental=.*/ro\.build\.version\.incremental=$3/" \
+number=$(date +%y.%m.%d)
+rebuild_number=`echo $number | grep -Eo '.{7}$' | sed 's/.0/./g'`
+
+cat $build_prop_file | sed -e "s/ro\.build\.version\.incremental=.*/ro\.build\.version\.incremental=$rebuild_numbers/" \
                       | sed -e "s/ro\.product\.mod_device=.*//" > $build_prop_file.new
 
 echo "ro.product.mod_device=$2" >> $build_prop_file.new
